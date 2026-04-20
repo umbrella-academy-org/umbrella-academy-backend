594c15a30931f2d8dd613cd12d0c8badb8d3c5b6
Goal651 <bugiriwilson651@gmail.com>
2026-04-20 07:37:59 +0200

    finalizing project

--- src/config/db.ts
+++ src/config/db.ts
@@ -6,9 +6,9 @@ dotenv.config();
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/dreamize');
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`❌ Error connecting to MongoDB: ${(error as Error).message}`);
+    console.error(`Error connecting to MongoDB: ${(error as Error).message}`);
     process.exit(1);
   }
 };
